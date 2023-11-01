0eb7da87f2536c592f9b6aa5802af62db721656d
Your Name <your_email@address>
2023-11-01 16:57:09 +0000

    basic data types in python

---
+++ second_program.py
@@ -0,0 +1,20 @@
+x = 10
+y = "10"
+z = 10.1
+
+sum1 = x + x
+sum2 = y + y
+print(sum1, sum2)
+print(type(x), type(y), type(z))
+
+student_grades = [9, "Hello", 7.5, [1, 2, 4.33]]
+print(student_grades)
+
+numbers_range = range(0, 11)
+print(numbers_range)
+
+numbers_range_list = list(range(0,11))
+print(numbers_range_list)
+
+numbers_range_list_steps = list(range(0,11, 2))
+print(numbers_range_list_steps)
